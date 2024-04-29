ffcd8bc3fd656af675416bd906df76dcf629686a
jun-hf <fong.junh@gmail.com>
2024-04-29 20:23:46 +0100

    create TCP server

--- p2p/TCPTransport.go
+++ p2p/TCPTransport.go
@@ -1,6 +1,10 @@
 package p2p
 
-import "net"
+import (
+	"errors"
+	"log"
+	"net"
+)
 
 type TCPTransportOpts struct {
 	ListenAddr string
@@ -13,4 +17,39 @@ type TCPTransport struct {
 	TCPTransportOpts
 	incomingRpc chan RPC
 	listener net.Listener
+}
+
+func NewTCPTransport(opts TCPTransportOpts) *TCPTransport {
+	return &TCPTransport{
+		TCPTransportOpts: opts,
+		incomingRpc: make(chan RPC),
+	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+	t.listener, err = net.Listen("tcp", t.ListenAddr)
+	if err != nil {
+		return err
+	}
+
+	go t.acceptLoop()
+
+	log.Println("Started tcp transport at:", t.listener.Addr())
+	return nil
+}
+
+func (t *TCPTransport) acceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if errors.Is(err, net.ErrClosed) {
+			log.Printf("Closing TCP server %v\n", t.ListenAddr)
+			return
+		}
+		if err != nil {
+			log.Printf("TCP server %v failed to accept connection: %v\n", t.ListenAddr, err)
+			continue
+		}
+		go t.handleConnection(conn, true)
+	}
 }
